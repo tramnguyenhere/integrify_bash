588a9d571cc6ef693bdef74e93ac1f61af1d7fa5
Tram Nguyen <tramngoc1402@gmail.com>
2023-08-23 13:11:29 +0300

    Assignment 1

---
+++ assignment1/script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [[ $# -eq 1 ]];
+then
+    cd $1
+    git shortlog -sn |
+    awk '{print $2" "$3" - "$1}' > ../output.txt
+elif [[ $# -eq 0 ]];
+then
+    echo "Error: File path is missing"
+    exit 1
+else
+    cd $1
+    for name in "$@" ; do
+        git shortlog -sn | grep "$name" |
+    awk '{print $2" "$3" - "$1}' >> ../output.txt
+    done
+fi
